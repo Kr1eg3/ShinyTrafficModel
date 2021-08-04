c67245da3d88c889b5b515909ac02ff2c085e94e
Arkadiy Pozharskiy <50653532+Kr1eg3@users.noreply.github.com>
2021-08-04 12:20:37 +0000

    переработал концепт дороги + докстринги

--- road.py
+++ road.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
 
+#Code written by 
+#       _          _   __
+#  ___ | |_  _ __ / | / _|  ___
+# / __|| __|| '__|| || |_  / _ \
+# \__ \| |_ | |   | ||  _||  __/
+# |___/ \__||_|   |_||_|   \___|
+#
+
+
 import numpy as np
 
 
 
 class Road(object):
+    """
+    A class that creates a matrix-road for the cellular automaton
 
+    ...
+
+    Attributes
+    ----------
+    road_length : int
+        the length of the road, and the number of columns in the matrix
+    numb_of_lines : int 
+        the number of lines at the road, as well as the number of matrix lines
+    numb_of_cars : int
+        the number of cars on the road, and the number of one's in the matrix
+
+    Methods
+    -------
+    generate_road(args):
+        Description
+    get_road_info(args)
+        Descriptionn
+
+    """
     def __init__(self, road_length, numb_of_lines, numb_of_cars):
         self.road_length = road_length
         self.numb_of_lines = numb_of_lines
